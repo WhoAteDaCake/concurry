74a795d82660d673d67a5717f96398fb0217f063
WhoAteDaCake <doitsimplefy@gmail.com>
2021-04-08 18:44:18 +0100

    (feat) save items

--- src/con-curry.cr
+++ src/con-curry.cr
@@ -1,12 +1,49 @@
-require "option_parser"
+require "commander"
+require "./config"
 
-def parse_cmd()
-  OptionParser.parse do |parser|
-    parser.banner = "Usage: con-curry [...cmd]"
+cli = Commander::Command.new do |cmd|
+  cmd.use  = "con-curry"
+  cmd.long = "con-curry: Program used to start concurrent processes"
 
-    parser.on("-h", "--help", "Show this help") do
-      puts parser
-      exit
+  cmd.flags.add do |flag|
+    flag.name        = "config"
+    flag.short       = "-c"
+    flag.long        = "--config"
+    flag.description = "YAML config file path"
+    flag.default     = "none"
+    flag.persistent  = true
+  end
+
+  cmd.run do |options, arguments|
+    cfg_path = options.string["config"]
+    if cfg_path == "none"
+      puts "Please specify config file via --config flag"
+      Process.exit
+    end
+
+    config = File.open(Path[cfg_path].expand) do |file|
+      Config.from_yaml(file)
+    end
+
+    env = ENV
+    # puts config.fill_arg("test: $DAPR_HTTP_PORT", ENV)
+    config.commands.each do | k, cmd |
+      args = cmd.arguments.reduce(Array(String).new) do | items, arg |
+        result = config.parse(arg, env)
+        case result
+          in Nil
+            items
+          in String
+            items << result
+            items
+          in Array(String)
+            items + result
+        end
+      end
+      puts args
     end
   end
+
 end
+
+Commander.run(cli, ARGV)
---
+++ src/config.cr
@@ -0,0 +1,59 @@
+require "yaml"
+
+ENV_VAR_RE = /\$(([A-Z]|_)+)/
+
+class Config
+  include YAML::Serializable
+
+  class Command 
+    include YAML::Serializable
+
+    class CheckedArgument
+      include YAML::Serializable
+
+      property when : String
+      property show : String | Array(String)
+    end
+
+    property command : String;
+    property arguments : Array(CheckedArgument | Array(String) | String)
+  end
+
+  property defaults : Hash(String, String)
+  property commands : Hash(String, Command)
+
+  def get_env(var, env)
+    env.fetch(var, @defaults[var]?)
+  end
+
+  def fill_arg(argument, env)
+    var = ENV_VAR_RE.match(argument).try &.[1]
+    if var.nil?
+      argument
+    else
+      value = get_env(var, env)
+      if value.nil?
+        puts "Missing env variable: [#{var}]"
+        Process.exit
+      else
+        argument.gsub("$" + var, value)
+      end
+    end
+  end
+
+  def parse(arg, env)
+    if arg.is_a?(String)
+      fill_arg(arg.as(String), env)
+    elsif arg.is_a?(Array(String))
+      arg.as(Array(String)).map { | a | fill_arg(a, env) }
+    else
+      spec = arg.as(Command::CheckedArgument)
+      # Remove $
+      if get_env(spec.when.delete_at(0), env).nil?
+        Nil
+      else
+        parse(spec.show, env)
+      end
+    end
+  end
+end
